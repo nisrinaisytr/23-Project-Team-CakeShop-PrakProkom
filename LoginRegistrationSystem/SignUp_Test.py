727799105d34827710979f82ac6beee2d5b85ae4
Nisrina hamidah isyaratri <nisrinaisyararti@gmail.com>
2024-05-22 15:06:58 +0700

    Modif warna

--- LoginRegistrationSystem/SignUp_Test.py
+++ LoginRegistrationSystem/SignUp_Test.py
@@ -9,7 +9,7 @@ import subprocess
 window = Tk()
 window.title("Sign Up")
 window.geometry('925x500+300+200')
-window.configure(bg='#fff')
+window.configure(bg='#FFDED9')
 window.resizable(False, False)
 
 def open_signin_window():
@@ -24,7 +24,8 @@ def signup():
     if password == confirm_password:
         try:
             # Check if CSV file exists
-            file_exists = os.path.exists('datasheet.csv')
+            cwd = os.getcwd()
+            file_exists = os.path.exists('{}\\datasheet.csv'.format(cwd))
             print(f"File exists: {file_exists}")
 
             # Open the CSV file in append mode
@@ -64,23 +65,23 @@ def signup():
 
 
 
-img_path = os.path.join('LoginRegistrationSystem', 'images', 'login.png')
+img_path = os.path.join('23-Project-Team-CakeShop-PrakProkom','LoginRegistrationSystem', 'images', 'logo.png')
 
 # Load and resize the image using Pillow
 if not os.path.exists(img_path):
     messagebox.showerror("Error", "Image file not found")
 else:
     img = Image.open(img_path)
-    img = img.resize((560, 360), Image.LANCZOS)  # Resize the image to fit the window
+    img = img.resize((360, 360), Image.LANCZOS)  # Resize the image to fit the window
     img = ImageTk.PhotoImage(img)
-    label_img = Label(window, image=img, border=0, bg='white')
+    label_img = Label(window, image=img, border=0, bg='#FFDED9')
     label_img.image = img  # Keep a reference to avoid garbage collection
-    label_img.place(x=-10, y=50) # To change position based on X & Y coordinates
+    label_img.place(x=60, y=50) # To change position based on X & Y coordinates
 
-frame = Frame(window, width=560, height=360, bg='#fff')
+frame = Frame(window, width=560, height=360, bg='#FFDED9')
 frame.place(x=480, y=50)
 
-heading = Label(frame, text="Sign Up", fg="#57a1f8", bg='white', font=('Microsoft Yahei UI Light', 23, 'bold'))
+heading = Label(frame, text="Sign Up", fg="#F16A6A", bg='#FFDED9', font=('Microsoft Yahei UI Light', 23, 'bold'))
 heading.place(x=100, y=5)
 
 def on_enter(e, widget, placeholder):
@@ -95,7 +96,7 @@ def on_leave(e, widget, placeholder):
         if widget == code or widget == confirm_code:
             widget.config(show='')
 
-user = Entry(frame, width=25, fg='black', border=0, bg='white', font=('Microsoft YaHei UI Light', 11))
+user = Entry(frame, width=25, fg='black', border=0, bg='#FFDED9', font=('Microsoft YaHei UI Light', 11))
 user.place(x=30, y=80)
 user.insert(0, 'Username')
 user.bind("<FocusIn>", lambda e: on_enter(e, user, 'Username'))
@@ -103,7 +104,7 @@ user.bind("<FocusOut>", lambda e: on_leave(e, user, 'Username'))
 
 Frame(frame, width=295, height=2, bg='black').place(x=25, y=107)
 
-code = Entry(frame, width=25, fg='black', border=0, bg='white', font=('Microsoft YaHei UI Light', 11))
+code = Entry(frame, width=25, fg='black', border=0, bg='#FFDED9', font=('Microsoft YaHei UI Light', 11))
 code.place(x=30, y=150)
 code.insert(0, 'Password')
 code.bind("<FocusIn>", lambda e: on_enter(e, code, 'Password'))
@@ -111,7 +112,7 @@ code.bind("<FocusOut>", lambda e: on_leave(e, code, 'Password'))
 
 Frame(frame, width=295, height=2, bg='black').place(x=25, y=177)
 
-confirm_code = Entry(frame, width=25, fg='black', border=0, bg='white', font=('Microsoft YaHei UI Light', 11))
+confirm_code = Entry(frame, width=25, fg='black', border=0, bg='#FFDED9', font=('Microsoft YaHei UI Light', 11))
 confirm_code.place(x=30, y=220)
 confirm_code.insert(0, 'Confirm Password')
 confirm_code.bind("<FocusIn>", lambda e: on_enter(e, confirm_code, 'Confirm Password'))
@@ -120,11 +121,11 @@ confirm_code.bind("<FocusOut>", lambda e: on_leave(e, confirm_code, 'Confirm Pas
 
 Frame(frame, width=295, height=2, bg='black').place(x=25, y=247)
 
-Button(frame, width=39, pady=7, text='Sign up', bg='#57a1f8', fg='white', border=0, command=signup).place(x=35, y=280)
-label = Label(frame, text='Already have an account?', fg='black', bg='white', font=('Microsoft YaHei UI Light', 9))
+Button(frame, width=39, pady=7, text='Sign up', bg='#F16A6A', fg='#FFDED9', border=0, command=signup).place(x=35, y=280)
+label = Label(frame, text='Already have an account?', fg='black', bg='#FFDED9', font=('Microsoft YaHei UI Light', 9))
 label.place(x=70, y=340)
 
-signin = Button(frame, width=6, text="Sign in", border=0, bg='white', cursor='hand2', fg='#57a1f8', command=open_signin_window)
+signin = Button(frame, width=6, text="Sign in", border=0, bg='#FFDED9', cursor='hand2', fg='#57a1f8', command=open_signin_window)
 signin.place(x=215, y=341)
 
 window.mainloop()
--- LoginRegistrationSystem/signin.py
+++ LoginRegistrationSystem/signin.py
@@ -7,7 +7,7 @@ import csv
 root=Tk()
 root.title('SignIn')
 root.geometry('925x500+300+200')
-root.configure(bg="#fff")
+root.configure(bg="#FFDED9")
 root.resizable(False,False)
 
 def open_signup_window():
@@ -18,10 +18,11 @@ def open_signup_window():
 def signin():
     username = user.get()
     password = code.get()
-
+    path = os.getcwd() + '\\datasheet.csv'
     # Load the data from the datasheet.csv file
     try:
-        with open('datasheet.csv', newline='') as file:
+        
+        with open(path, newline='') as file:
             reader = csv.reader(file)
             credentials = {rows[0]: rows[1] for rows in reader}
     except Exception as e:
@@ -34,23 +35,23 @@ def signin():
     else:
         messagebox.showerror("Error", "Invalid username or password")
 
-img_path = os.path.join('LoginRegistrationSystem', 'images', 'login.png')
+img_path = os.path.join('23-Project-Team-CakeShop-PrakProkom','LoginRegistrationSystem', 'images', 'logo.png')
 
 # Load and resize the image using Pillow
 if not os.path.exists(img_path):
     messagebox.showerror("Error", "Image file not found")
 else:
     img = Image.open(img_path)
-    img = img.resize((560, 360), Image.LANCZOS)  # Resize the image to fit the window
+    img = img.resize((360, 360), Image.LANCZOS)  # Resize the image to fit the window
     img = ImageTk.PhotoImage(img)
-    label_img = Label(root, image=img, border=0, bg='white')
+    label_img = Label(root, image=img, border=0, bg='#FFDED9')
     label_img.image = img  # Keep a reference to avoid garbage collection
-    label_img.place(x=-10, y=50) # To change position based on X & Y coordinates
+    label_img.place(x=60, y=50) # To change position based on X & Y coordinates
 
-frame=Frame(root,width=350,height=350,bg='white')
+frame=Frame(root,width=350,height=350,bg='#FFDED9')
 frame.place(x=480,y=70)
 
-heading=Label(frame,text='Sign in', fg='#544e4a',bg='white',font=('Microsoft YaHei UI Light',23,'bold'))
+heading=Label(frame,text='Sign in', fg='#F16A6A',bg='#FFDED9',font=('Microsoft YaHei UI Light',23,'bold'))
 heading.place(x=100,y=5)
 
 ############----------------------------------------------------
@@ -76,7 +77,7 @@ def on_leave(e, widget, placeholder):
 #     if name=='':
 #         user.insert(0,'Username')
 
-user=Entry(frame,width=36,fg='black',border=0,bg='white',font=('Microsoft YaHei UI Light',11))
+user=Entry(frame,width=36,fg='black',border=0,bg='#FFDED9',font=('Microsoft YaHei UI Light',11))
 user.place(x=30,y=80)
 user.insert(0,'Username')
 # user.bind('<FocusIn>', on_enter)
@@ -96,7 +97,7 @@ Frame(frame,width=295,height=2,bg='black').place(x=25,y=107)
 #     if name=='':
 #         code.insert(0,'Password') 
 
-code=Entry(frame,width=36,fg='black',border=0,bg='white',font=('Microsoft YaHei UI Light',11))
+code=Entry(frame,width=36,fg='black',border=0,bg='#FFDED9',font=('Microsoft YaHei UI Light',11))
 code.place(x=30,y=150)
 code.insert(0,'Password')
 # code.bind('<FocusIn>', on_enter)
@@ -108,11 +109,11 @@ Frame(frame,width=295,height=2,bg='black').place(x=25,y=177)
 
 #################################################################
 
-Button(frame,width=39,pady=7,text='Sign in',bg='#57a1f8',fg='white',border=0,command=signin).place(x=35,y=204)
-label=Label(frame,text="Don't have an account?",fg='black',bg='white',font=('Microsoft YaHei UI Light', 9))
+Button(frame,width=39,pady=7,text='Sign in',bg='#F16A6A',fg='#FFDED9',border=0,command=signin).place(x=35,y=204)
+label=Label(frame,text="Don't have an account?",fg='black',bg='#FFDED9',font=('Microsoft YaHei UI Light', 9))
 label.place(x=75,y=270)
 
-sign_up=Button(frame,width=6,text='Sign up',border=0,bg='white',cursor='hand2',fg='#57a1f8', command=open_signup_window)
+sign_up=Button(frame,width=6,text='Sign up',border=0,bg='#FFDED9',cursor='hand2',fg='#57a1f8', command=open_signup_window)
 sign_up.place(x=215,y=270)
 
 root.mainloop()
